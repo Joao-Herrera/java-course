733b3b200fb8b941d95a627136b40e643df0a30d
Edgar Herrera <edgarjoao.herrera@globant.com>
2026-02-20 13:44:43 -0600

    updating more commands

--- JavaExercises/grader_with_if_and_char.java
+++ JavaExercises/grader_with_if_and_char.java
@@ -4,7 +4,9 @@ public class grader_with_if_and_char {
     public static void main(String[] args) { 
         // Integer.parseInt(args[0]) allows all grades to be tested
         // Do not change this line:
-        int studentScore = Integer.parseInt(args[0]);
+        //int studentScore = Integer.parseInt(args[0]);
+
+        int studentScore = 82;
         
         char grade;
 
@@ -12,7 +14,10 @@ public class grader_with_if_and_char {
 
         if (studentScore >= 90) {
             grade = 'A';
-        } else if (studentScore >= 80) {
+                    System.out.println(studentScore);
+
+        } 
+        else if (studentScore >= 80) {
             grade = 'B';
         } else if (studentScore >= 70) {
             grade = 'C';
