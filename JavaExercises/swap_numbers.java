5546016cc595f9c09f3fa76b32cee9f06e4c70d0
Edgar Herrera <edgarjoao.herrera@globant.com>
2026-02-24 16:17:50 -0600

    nuevos cambios de identacion

--- JavaExercises/swap_numbers.java
+++ JavaExercises/swap_numbers.java
@@ -1,6 +1,10 @@
 package JavaExercises;
 
 public class swap_numbers {
+
+    // 2 formas de poder hacer un "swap" con numeros
+
+    // 1ra forma, sumando y retando cada uno de los valores
     public static void main(String[] args) {
         
         int a = 10;
@@ -13,12 +17,14 @@ public class swap_numbers {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
     
+    // 2da forma, creando una variable adicional "temp" para poder igualarla con cada uno de los valores.
 
-    int x = 10;
-    int y = 20;
-    int temp = x;
-    x = y;
-    y = temp;
+        int x = 10;
+        int y = 20;
+        int temp = x;
+
+        x = y;
+        y = temp;
 
     System.out.println("x = " + x);
     System.out.println("y = " + y);
