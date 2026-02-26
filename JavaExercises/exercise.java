73702310f81acbefd40dd8516435a35d7bc894f9
Edgar Herrera <edgarjoao.herrera@globant.com>
2026-02-26 15:53:41 -0600

    updating exercises

---
+++ JavaExercises/exercise.java
@@ -0,0 +1,9 @@
+package JavaExercises;
+
+public class exercise {
+    public static void main(String[] args) {
+        String x = "hey";
+        x = "hoy";
+        System.out.println(x);
+    }
+}
